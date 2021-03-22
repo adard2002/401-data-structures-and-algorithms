0bfce5c929bd0ceca648d93d2d4b880f37c57675
unknown <adard2002@gmail.com>
2021-03-22 16:24:22 -0500

    Array Reverse Challenge Done

--- Challenges/Program.cs
+++ Challenges/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Challenges.ArrayChallenges;
+using System;
+
 
 namespace Challenges
 {
@@ -6,7 +8,13 @@ namespace Challenges
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Array Reverse:");
+            
+            int[] exampleArray1 = new int[] { 1, 2, 3, 4, 5, 6 };
+            int[] reversedArray1 = ArrayReverse.Reverse(exampleArray1);
+            Console.WriteLine("Should be 6, 5, 4, 3, 2, 1: {0}", string.Join(", ", reversedArray1));
+
+
         }
     }
 }
