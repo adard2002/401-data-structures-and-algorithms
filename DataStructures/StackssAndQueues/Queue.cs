68e41ba61ffbcf6e2a1dff495b33e3b6896c1c3b
unknown <adard2002@gmail.com>
2021-04-02 14:50:15 -0500

    added Iqueue and Istack code

--- DataStructures/StackssAndQueues/Queue.cs
+++ DataStructures/StackssAndQueues/Queue.cs
@@ -4,15 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Challenges
+namespace DataStructures.StackssAndQueues
 {
-    class ZipTwoLink
+    class Class1
     {
-
-
-
-
-
-
     }
 }
---
+++ StacksAndQueues.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class StacksAndQueues
+{
+	public StacksAndQueues()
+	{
+
+		interface IStack<T>
+		{
+			void Push(T value);
+			T Pop();
+			T Peek();
+			bool IsEmpty { get; }
+		}
+
+
+
+		interface IQueue<T>
+		{
+			void Enqueue(T value);
+			T Dequeue();
+			T Peek();
+			bool IsEmpty { get; }
+		}
+
+
+
+
+	}
+}
